6d8c58d0445e0f525cb3fe3ebb1e8b093eefb8ee
Greg <com@ccgr.fr>
2022-06-05 16:13:10 +0200

    Mon 1er commit

---
+++ create-git-workspace.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+echo "===== Git Init ====="
+git init
+
+echo "===== Git config ====="
+git config --global user.name "Greg"
+git config --global user.email "com@ccgr.fr"
+git config --global color.ui true
