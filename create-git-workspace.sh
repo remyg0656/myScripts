f12730c2db37f0f828f479a398ce1a1fb7807934
Ordi Alien <com@ccgr.fr>
2022-06-06 04:07:23 +0200

    On met la config pour PetitClevo

--- create-git-workspace.sh
+++ create-git-workspace.sh
@@ -5,7 +5,7 @@ git init
 
 echo "===== Git config ====="
 git config --global user.name "PetitClevo"
-git config --global user.email "com@ccgr.fr"
+git config --global user.email "PetitClevo@ccgr.fr"
 git config --global color.ui true
 
 echo ">>>>>>> fin de $0"
--- readme.txt
+++ readme.txt
@@ -2,5 +2,9 @@ C'est ici qu'on va rajouter les trucs a faire
 
 Liste des scripts:
 	* create-git-workpsace.sh
+
+
+Acteur:
+	* PetitClevo
 	
 ===== Ligne de fin du readme.txt =====
