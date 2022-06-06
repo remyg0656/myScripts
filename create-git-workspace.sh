4671a0dd4b4268904afc15e71678a8dc634cc46b
Ordi Alien <com@ccgr.fr>
2022-06-06 02:39:09 +0200

    Point de depart de l'alien2

--- create-git-workspace.sh
+++ create-git-workspace.sh
@@ -4,7 +4,7 @@ echo "===== Git Init ====="
 git init
 
 echo "===== Git config ====="
-git config --global user.name "Greg"
+git config --global user.name "OrdiAlien"
 git config --global user.email "com@ccgr.fr"
 git config --global color.ui true
 
--- readme.txt
+++ readme.txt
@@ -3,4 +3,7 @@ C'est ici qu'on va rajouter les trucs a faire
 Liste des scripts:
 	* create-git-workpsace.sh
 	
+Liste des acteurs:
+	* OrdiAlien entre dans la course
+
 ===== Ligne de fin du readme.txt =====
