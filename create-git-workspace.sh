742f45841300fe5047404de7afbefb96c1fe928f
ordiAlien <ordiAlien@ccgr.fr>
2022-06-07 02:29:34 +0200

    orDiAlien tente de revenir dans la course

---
+++ create-git-workspace.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+echo "===== Git Init ====="
+git init
+
+echo "===== Git config ====="
+git config --global user.name "ordiAlien"
+git config --global user.email "ordiAlien@ccgr.fr"
+
+git config --global color.ui true
+
+echo ">>>>>>> fin de $0"
